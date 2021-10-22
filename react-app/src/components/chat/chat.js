0c5e71355e6a0180fe622f8c69ff5b0150d243ef
Peter Joh <johpeter@gmail.com>
2021-10-22 11:08:05 -0600

    test

--- react-app/src/components/chat/chat.js
+++ react-app/src/components/chat/chat.js
@@ -12,7 +12,7 @@ const Chat = () => {
     useEffect(() => {
         // open socket connection
         // create websocket
-        socket = io('https://peterpoker.herokuapp.com/');
+        socket = io('https://peterpoker.herokuapp.com');
 
         socket.on("chat", (chat) => {
             setMessages(messages => [...messages, chat])
