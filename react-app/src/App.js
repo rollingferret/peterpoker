f729f537c133cbac101bb2302ba56a47950c3154
Peter Joh <johpeter@gmail.com>
2021-10-24 10:58:38 -0600

    fixed small bug new starting point

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -47,7 +47,7 @@ function App() {
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
+        <ProtectedRoute path='/home' exact={true} >
           <h1>My Home Page</h1>
         </ProtectedRoute>
       </Switch>
