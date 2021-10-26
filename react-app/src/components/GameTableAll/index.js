f3928faa833a9749431b6008da1c8cf65e9d0256
Peter Joh <johpeter@gmail.com>
2021-10-26 05:21:11 -0600

    almost broke backend init

--- react-app/src/components/GameTableAll/index.js
+++ react-app/src/components/GameTableAll/index.js
@@ -1,27 +1,23 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getAllGametablesThunk } from "../../store/gametables";
 
 
 function GetAllGameTables() {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.session.user);
 
-//   useEffect(() => {
-//     dispatch(getCommentByIdThunk(imageId));
-//   }, [dispatch, imageId]);
+  useEffect(() => {
+    dispatch(getAllGametablesThunk());
+  }, [dispatch]);
 
-//   const commentList = useSelector((state) => {
-//     return state.comments;
-//   });
-//   let currentComments;
-//   if (commentList) {
-//     currentComments = Object.values(commentList).filter(
-//       (comment) => comment.image_id === +imageId
-//     );
+  const gameTableList = useSelector((state) => {
+    return state.gametables;
+  });
 
-//     const commentsSection = Object.values(currentComments)?.map((comment) => {
-//       const newDate = comment?.created_at.split(" ");
+  console.log(gameTableList, '888888888888888888888888888888888')
+
 
       return (
 
