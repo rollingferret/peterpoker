6ad61f65a24936bc41817118943d70d567c3a55f
Peter Joh <johpeter@gmail.com>
2021-10-31 09:57:37 -0600

    more css

--- react-app/src/components/Follow/index.js
+++ react-app/src/components/Follow/index.js
@@ -32,14 +32,14 @@ const FollowButton = ({ userId }) => {
     <>
       {isFollowing && (
         <div className={css.follow_button_div1}>
-          <div className={css.follow_button} onClick={unfollowUser}>
+          <div className={css.follow_button1} onClick={unfollowUser}>
             Unfollow
           </div>
         </div>
       )}
       {!isFollowing && (
         <div className={css.follow_button_div2}>
-          <div className={css.follow_button} onClick={followUser}>
+          <div className={css.follow_button2} onClick={followUser}>
             Follow
           </div>
         </div>
